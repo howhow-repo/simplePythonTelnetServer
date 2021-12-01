44a6dd345c4851f5e28dac7727866d350f084ec6
Howard Hsu <howard.hsu1129@gmail.com>
2021-12-01 16:46:43 +0800

    edit ui

--- main.py
+++ main.py
@@ -7,17 +7,32 @@ from time import ctime
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         cur = threading.current_thread()
+        peer_name = self.request.getpeername()
         print(f'[{ctime()}] Client connected from {self.request.getpeername()} and [{cur.name}] is handling with him.')
+        self.request.send(str.encode("You can close thisd connection by ctrl+c or 'exit':\n"))
+        self.request.send(str.encode("<<<< sent: "))
         while True:
-            indata = self.request.recv(1024).strip()
-            if len(indata) == 0:  # connection closed
-                self.request.close()
-                print('client closed connection.')
-                break
-            print(f'rev: {indata.decode()}')
+            try:
+                indata = self.request.recv(1024).strip()
+                if indata == b'\xff\xf4\xff\xfd\x06' or indata.decode() == 'exit':  # connection closed
+                    self.request.close()
+                    print('client closed connection.')
+                    break
+                print(f'{peer_name} rev: {indata.decode()}')
 
-            outdata = f'echo {indata.decode()}\n'
-            self.request.send(outdata.encode())
+                outdata = f'>>>> resp: echo: {indata.decode()}\n'\
+                          f'<<<< sent: '
+
+                self.request.send(outdata.encode())
+            except ConnectionResetError:
+                print(f"{peer_name} Connection reset by peer")
+                break
+            except BrokenPipeError:
+                print(f"---- {peer_name} Broken pipe, exit thread ----")
+                break
+            except OSError as e:
+                print(f"{e}")
+                break
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
